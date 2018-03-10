2014b3cc01fea1e3b52d9dcd5574bfe6d1905f2c
Sebastian Oberschwendtner <sebastian.oberschwendtner@tum.de>
2018-03-10 14:30:56 +0100

    Deleted unnecessary files for commit

--- Debug/driver/i2c.d
+++
@@ -1,1 +0,0 @@
-driver/i2c.o: ../driver/i2c.c
--- Debug/src/main.d
+++
@@ -1,7 +0,0 @@
-src/main.o: ../src/main.c \
- /Volumes/Macintosh\ HD/Documents/Eclipse/OTP15_oVario/inc/oVario_Framework.h \
- /Volumes/Macintosh\ HD/Documents/Eclipse/OTP15_oVario/driver/i2c.h
-
-/Volumes/Macintosh\ HD/Documents/Eclipse/OTP15_oVario/inc/oVario_Framework.h:
-
-/Volumes/Macintosh\ HD/Documents/Eclipse/OTP15_oVario/driver/i2c.h:
