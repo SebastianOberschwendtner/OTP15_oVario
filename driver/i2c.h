e51efdff546cce419ca7d20f7d17ec4af796fff9
Sebastian Oberschwendtner <sebastian.oberschwendtner@tum.de>
2018-05-11 22:18:27 +0200

    Added register write functions to the i2c driver.

--- driver/i2c.h
+++ driver/i2c.h
@@ -12,6 +12,7 @@
 // ***** Defines *****
 //I2C addresses of sensors
 #define i2c_addr_MS5611 0xEE
+#define i2c_addr_BMS	0x12
 
 
 // ***** Functions *****
@@ -19,6 +20,7 @@ void init_i2c(void);
 void i2c_send_char(unsigned char ch_address, unsigned char ch_data);
 void i2c_send_int(unsigned char ch_address, unsigned int i_data);
 void i2c_send_int_LSB(unsigned char ch_address, unsigned int i_data);
+void i2c_send_int_register_LSB(unsigned char ch_address, unsigned char ch_register, unsigned int i_data);
 unsigned char i2c_read_char(unsigned char ch_address, unsigned char ch_command);
 unsigned int i2c_read_int(unsigned char ch_address, unsigned char ch_command);
 unsigned int i2c_read_int_LSB(unsigned char ch_address, unsigned char ch_command);
