42d4aae2072a948fb010d093a81f453847702475
Sebastian Oberschwendtner <sebastian.oberschwendtner@tum.de>
2018-05-20 19:43:43 +0200

    Commit of main with bug in memory allocation.

--- src/main.c
+++ src/main.c
@@ -11,26 +11,21 @@
 #include "oVario_Framework.h"
 #include "DOGXL240.h"
 #include "ipc.h"
-#include "sound.h"
-#include "exti.h"
-#include "timer.h"
+//#include "sound.h"
+//#include "exti.h"
+//#include "timer.h"
 #include "i2c.h"
-#include "ms5611.h"
-#include "datafusion.h"
+//#include "ms5611.h"
+//#include "datafusion.h"
 #include "Variables.h"
+#include "BMS.h"
+#include "sdio.h"
 
 uint32_t error_var = 0;
 unsigned long l_count_tick = 0;
+unsigned int i_temp = 0;
 
-int32_t testmain = 0;
-
-uint8_t volu;
-uint16_t freque;
-uint8_t peri;
-float temp_climb;
-
-datafusion_T* p_ipc_df_data;
-T_sound_command main_command;
+BMS_T* tBMS;
 
 int main(void)
 {
@@ -38,88 +33,74 @@ int main(void)
 	init_systick_ms(SYSTICK);
 	init_led();
 
-	set_led_red(ON);
-	init_lcd();
-	exti_init();
-	sound_init();
-	timer_init();
 	init_i2c();
-	MS5611_init();
-	datafusion_init();
+
+	wait_systick(30);
+
+	init_lcd();
+	init_BMS();
+
+	//init_sdio();
+	//exti_init();
+	//sound_init();
+	//timer_init();
+	//MS5611_init();
+
+	BMS_adc_start();
+
+	wait_systick(1);
 
 
-
-	p_ipc_df_data = ipc_memory_get(did_DATAFUSION);
-
+	tBMS = ipc_memory_get(did_BMS);
+	BMS_get_status();
+	//BMS_set_otg(ON);
 
 	while(1)
 	{
 		if(TICK_PASSED)
 		{
-			sound_task();
-			ms5611_task();
-			datafusion_task();
 
+			lcd_set_cursor(0,16);
+			lcd_string2buffer("Bat Voltage: ");
+			lcd_num2buffer(tBMS->battery_voltage,5);
+			lcd_string2buffer("mV");
 
+			lcd_set_cursor(0,32);
+			lcd_string2buffer("USB Voltage: ");
+			lcd_num2buffer(tBMS->input_voltage,5);
+			lcd_string2buffer("mV");
 
+			lcd_set_cursor(0,48);
+			lcd_string2buffer("Charge:      ");
+			lcd_num2buffer(tBMS->charge_current,5);
+			lcd_string2buffer("mA");
 
-			temp_climb = p_ipc_df_data->climbrate_filt;
+			lcd_set_cursor(0,64);
+			lcd_string2buffer("Current:    ");
+			lcd_signed_num2buffer(tBMS->current,5);
+			lcd_string2buffer("mA");
 
-			if(temp_climb > 5)
-				temp_climb = 5;
+			lcd_set_cursor(0,80);
+			lcd_string2buffer("USB Current: ");
+			lcd_num2buffer(tBMS->input_current,5);
+			lcd_string2buffer("mA");
 
-			if(temp_climb < -2)
-				temp_climb = -2;
+			lcd_set_cursor(0,96);
+			lcd_string2buffer("Discharged: ");
+			lcd_signed_num2buffer(tBMS->discharged_capacity,4);
+			lcd_string2buffer("mAh");
 
-
-			/*
- Top:
- peri = 20;
- Sound = 2000;
-
- Bottom:
- peri = 100
- sound = 500;
-
-			 */
-
-
-
-
-
-
-
-			if(temp_climb > 0.3)
-			{
-				main_command.command 	= sound_cmd_set_unmute;
-				main_command.data		= 0;
-				ipc_queue_push(&main_command, 5, did_SOUND);
-
-				freque = (uint16_t)((temp_climb)/5*1500 + 500);
-				volu = 100;
-				if(temp_climb < 1)
-					temp_climb = 1;
-				peri = (uint8_t)(100 - (temp_climb/5*80));
-
-				// Set Frequency
-				main_command.command 	= sound_cmd_set_frequ;
-				main_command.data		= (uint32_t)freque;
-
-				ipc_queue_push(&main_command, 5, did_SOUND);
-
-				// Set Period
-				main_command.command 	= sound_cmd_set_period;
-				main_command.data		= (uint32_t)peri;
-
-				ipc_queue_push(&main_command, 5, did_SOUND);
-			}
+			lcd_set_cursor(0,112);
+			lcd_string2buffer("State: ");
+			if(tBMS->charging_state & STATUS_FAST_CHARGE)
+				lcd_string2buffer("Charging");
 			else
-			{
-				main_command.command 	= sound_cmd_set_mute;
-				main_command.data		= 0;
-				ipc_queue_push(&main_command, 5, did_SOUND);
-			}
+				lcd_string2buffer("Charged ");
 
+			lcd_send_buffer();
+			BMS_get_adc();
+			BMS_gauge_get_adc();
+			BMS_charge_start();
 			set_led_green(TOGGLE);
 			l_count_tick++;
 			if(l_count_tick == 5)
